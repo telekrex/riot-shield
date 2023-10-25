3c0dde9740b6c9e1f2288723a46313cc011149f2
telekrex <telekrex@gmail.com>
2023-10-25 15:30:46 -0400

    naming conv edit

--- source/mute.py
+++ source/mute.py
@@ -3,27 +3,27 @@ import sys
 from platform import system
 
 if system() == 'Windows':
-    hosts = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+    HOSTS = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
 else:
-    hosts = '/etc/hosts'
+    HOSTS = '/etc/hosts'
 
 try:
     # if the provided arg is to "enable" the muting, (forced lowercase)
     # then set target variable to the content of the 'hosts muted' file
     if str(sys.argv[1]).lower() in ['enable', 'start', 'on']:
-        target = 'hosts muted'
+        TARGET = 'hosts muted'
 
     else:
         # if anything other than "enable" was asked for in arg,
         # then we assume "disable" and set target to content
         # of 'hosts default' file
-        target = 'hosts default'
+        TARGET = 'hosts default'
 
     # with target set, we open the system hosts file
     # and replace it with the content of target
-    with open(target, 'r') as file:
+    with open(TARGET, 'r') as file:
         contents = file.read()
-    with open(hosts, 'w') as file:
+    with open(HOSTS, 'w') as file:
         file.write(contents)
     # program will exit when finished
 
