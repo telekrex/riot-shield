59e5cf93b6ae693ec60937eda516512e7b3ba613
telekrex <telekrex@gmail.com>
2023-10-27 14:02:49 -0400

    fix rename, README

--- silencer.py
+++ silencer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Author: telekrex
 import sys
 from platform import system
 
@@ -9,7 +10,7 @@ else:
 
 try:
     # if the provided arg is to "enable" the muting, (forced lowercase)
-    # then set target variable to the content of the 'hosts muted' file
+    # then set target variable to the content of the 'hosts silenced' file
     if str(sys.argv[1]).lower() in ['enable', 'start', 'on']:
         TARGET = 'hosts silenced'
 
